692134205c848b730bfbbf86c825fe0af6dc4cfd
Vlad Flore <vlad.flore@exb.de>
2021-12-17 13:07:45 +0100

    Add guide project skeleton

---
+++ guides/building_a_hypermedia-driven_restful_web_service/src/main/java/com/example/building_a_hypermediadriven_restful_web_service/BuildingAHypermediaDrivenRestfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.building_a_hypermediadriven_restful_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingAHypermediaDrivenRestfulWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildingAHypermediaDrivenRestfulWebServiceApplication.class, args);
+    }
+
+}
---
+++ guides/building_a_hypermedia-driven_restful_web_service/src/test/java/com/example/building_a_hypermediadriven_restful_web_service/BuildingAHypermediaDrivenRestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.building_a_hypermediadriven_restful_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingAHypermediaDrivenRestfulWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
